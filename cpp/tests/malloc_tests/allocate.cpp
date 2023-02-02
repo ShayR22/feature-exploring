b2e7ce4e5939274f6296e9feb2bc085d1f1706ed
Shay <shay@DESKTOP-52PE1SP.localdomain>
2023-02-02 07:55:58 +0200

    cpp allocate test: add access
    in order to improve test add acceess to the
    allocations

--- cpp/tests/malloc_tests/allocate.cpp
+++ cpp/tests/malloc_tests/allocate.cpp
@@ -3,20 +3,32 @@
 #include <vector>
 #include <memory>
 
+static inline void sum_container(const auto& container, const uint32_t size) {
+    uint64_t sum = 0;
+    for (uint32_t i = 0; i < size; i++) {
+        sum += container[i];
+    }
 
+    if (sum > 0xFFFFFFFFULL) {
+        printf("sum=[%lu]\n", sum);
+    }
+}
 
 static void run_allocation_tests(uint32_t allocation_size) {
     auto allocate_vec_resize = [&]() {
         std::vector<uint32_t> vec;
         vec.resize(allocation_size);
+        sum_container(vec, allocation_size);
     };
 
     auto allocate_vec_ctor = [&]() {
         std::vector<uint32_t> vec(allocation_size);
+        sum_container(vec, allocation_size);
     };
 
     auto allocate_buffer = [&]() {
         auto buffer = std::make_unique<uint32_t[]>(allocation_size);
+        sum_container(buffer, allocation_size);
     };
 
     auto allocate_stack = [&]() {
@@ -24,8 +36,18 @@ static void run_allocation_tests(uint32_t allocation_size) {
             return;
         }
         uint8_t buffer[allocation_size];
+        sum_container(reinterpret_cast<uint8_t*>(buffer), allocation_size);
     };
 
+    std::vector<uint32_t> sum_vec(allocation_size);
+    for (uint32_t i = 0; i < allocation_size; i++) {
+        sum_vec[i] = i;
+    }
+    auto container_sum = [&]() {
+        sum_container(sum_vec, allocation_size);
+    };
+
+    RUN_TEST(container_sum);
     RUN_TEST(allocate_vec_resize);
     RUN_TEST(allocate_vec_ctor);
     RUN_TEST(allocate_buffer);
