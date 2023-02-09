7670829b4d7a1cd5fa771598cca9d157826c3da3
Shay <shay@DESKTOP-52PE1SP.localdomain>
2023-02-09 04:43:50 +0200

    concepts: recursive vs fold variadic

--- cpp/concepts/callable.hpp
+++
@@ -1,6 +0,0 @@
-
-
-// template <typename... Args, std::invocable<Args...> F>
-// void my_func(F&& f, Args&&... args) {
-//     f(args..);
-// }
--- cpp/concepts/main.cpp
+++ cpp/concepts/main.cpp
@@ -1,40 +1,66 @@
 #include <stdio.h>
 #include <string>
+#include <concepts>
+#include "spdlog/spdlog.h"
 
-#include "callable.hpp"
 
-#define PRINT_LINE(s) printf("%s: %s\n", __func__, s.c_str());
 
-int f(const std::string& s) {
-    PRINT_LINE(s)
-    return std::atoi(s.c_str());
+// ==============================================================================
+// ========================= FOLD EXPRESSION VARIADIC ===========================
+// ==============================================================================
+
+static int fold_expression_variadic_i = 1;
+static int f_print_i = 1;
+
+template<typename ...Args>
+concept MoreThanOneElement = requires(Args ... args) {
+    requires sizeof...(args) > 1;
+};
+
+template<typename T>
+void f_print(const T& element) {
+    spdlog::info("{}: {}", f_print_i++, element);
+}
+
+template<typename... Args>
+requires MoreThanOneElement<Args...>
+void f_print(Args&&... args) {
+    printf("fold_expression_variadic_i=%d\n", fold_expression_variadic_i++);
+    // cpp17 fold expression syntax (avoid recursive calls), args is like a pointer va_list,
+    // the entire expresion is inside a callable '()' and ... advance
+    // the args to the next argument until no args are left
+    (f_print(args), ...);
 }
 
 
-std::string f2(const std::string& s) {
-    PRINT_LINE(s)
-    return s;
+// ==============================================================================
+// ========================= RECURSIVE EXPRESSION VARIADIC ======================
+// ==============================================================================
+
+static int recursive_variadic_i = 1;
+static int r_print_i = 1;
+
+template<typename T>
+void r_print(const T& element) {
+    spdlog::info("{}: {}", r_print_i++, element);
 }
 
-
-template <typename... Args, std::invocable<Args...> F>
-void t_wrapper(F&& f, Args&&... args) {
-    int rv = f(args...);
-    printf("rv = %d\n", rv);
+// cant use move semantics in order to enforce 0 types (const & instead of &&)
+template<typename FirstArg, typename... RestArgs>
+void r_print(const FirstArg& arg, const RestArgs&... args) {
+    printf("recursive_variadic_i=%d\n", recursive_variadic_i++);
+    r_print(arg);
+    r_print(args...);
 }
 
-// void wrapper(std::invocable<int, const std::string&> auto&& fn, std::string& s) {
-//     PRINT_LINE(s)
-//     int rv = fn(s);
-//     printf("rv = %d\n", rv);
-// }
+// ==============================================================================
+// ================================== MAIN ======================================
+// ==============================================================================
 
 
 int main() {
-    std::string s = "42";
-    // wrapper(f, s);
-
-    // t_wrapper(f, s);
-    t_wrapper(f2, s);
+    f_print(42, 1, 3, "gg", std::string("wp"));
+    printf("========================================\n");
+    r_print(42, 1, 3, "gg", std::string("wp"));
     return 0;
 }
