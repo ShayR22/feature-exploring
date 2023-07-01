b2e91ad82f25ad2fa905cdaaa488cd5803b407c4
shayr <shayrashinsky@gmail.com>
2023-07-01 19:28:34 +0300

    type_erasure: shapes: add: copy and move semantics

--- cpp/type_erasure/shapes/circle.hpp
+++ cpp/type_erasure/shapes/circle.hpp
@@ -2,7 +2,13 @@
 
 class Circle {
 public:
+    Circle() : _radius{0} {}
     Circle(int radius) : _radius{radius} {}
+    Circle(const Circle& lhs) = default;
+    Circle(Circle&& rhs) = default;
+    Circle& operator=(const Circle& lhs) = default;
+    Circle& operator=(Circle&& rhs) = default;
+
     int get_radius() const { return _radius; }
 
 private:
--- cpp/type_erasure/shapes/rect.hpp
+++ cpp/type_erasure/shapes/rect.hpp
@@ -2,7 +2,12 @@
 
 class Rect {
 public:
+    Rect() : _w{0}, _h{0} {}
     Rect(int w, int h) : _w{w}, _h{h} {}
+    Rect(const Rect& lhs) = default;
+    Rect(Rect&& rhs) = default;
+    Rect& operator=(const Rect& lhs) = default;
+    Rect& operator=(Rect&& rhs) = default;
     int get_w() const { return _w; }
     int get_h() const { return _h; }
 
