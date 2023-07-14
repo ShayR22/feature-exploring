fae120f459f8a89a08ea879bfbdda90865289e24
shayr <shayrashinsky@gmail.com>
2023-07-14 17:52:46 +0300

    cpp: iwyu: add parsing to bash script

--- cpp/include_what_you_use/run_on_current_files.sh
+++ cpp/include_what_you_use/run_on_current_files.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-for f in `find ./ -maxdepth 1 -name "*.cpp" -o -name "*.h" -o -name "*.hpp"`; do 
-	echo -e "========== START $f =========="; 
-	iwyu $f -Iclang_includes
-	echo -e "========== END $f =============\n\n\n"; 
+OUTPUT_FILE="iwyu-output-file"
+
+if [[ -f "$OUTPUT_FILE" ]]; then
+	rm "$OUTPUT_FILE"
+fi
+
+
+for f in `find ./ -maxdepth 1 -name "*.cpp" -o -name "*.h" -o -name "*.hpp"`; do
+	echo -e "========== START $f ==========";
+	iwyu $f -Iclang_includes 2>&1 | tee -a $OUTPUT_FILE
+	echo -e "========== END $f =============\n\n\n";
 done
+
+echo -e "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+grep -rn $OUTPUT_FILE -e 'should remove these lines:\|- #include'
+rm $OUTPUT_FILE
