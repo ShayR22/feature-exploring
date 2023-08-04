9aa4050faa44206647d392639b86522b6dea9eac
shayr <shayrashinsky@gmail.com>
2023-08-04 21:33:09 +0300

    cpp: cmake_multi_libs: refactor scripts and scope

--- cpp/cmake_multiple_lib_deps/build.sh
+++ cpp/cmake_multiple_lib_deps/build.sh
@@ -1,7 +1,44 @@
 #!/bin/bash
 
-rm -rf build
-mkdir build
+
+IS_CLEAN=0
+IS_BUILD=0
+IS_INSTALL=0
+
+# Process arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -c | --clean)
+            IS_CLEAN=1
+            ;;
+        -b | --build)
+            IS_BUILD=1
+            ;;
+        -i | --install)
+            IS_INSTALL=1
+            ;;
+        *)
+            echo "Invalid argument: $1"
+            echo "Usage: $0 [-c|--clean]"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+
+if [ -d "build" ]; then
+    echo "Found build directory under build"
+    if [ "$IS_CLEAN" -ne 0 ]; then
+        echo "clean flag is on going to delete dir and recreate it"
+        rm -r "build"
+        mkdir "build"
+    fi
+
+else
+    echo "Build directory does not exist. creating build directory"
+    mkdir "build"
+fi
 
 pushd ./build
 
@@ -10,8 +47,15 @@ export CXX="/usr/bin/clang++"
 
 echo "Generating CMake"
 cmake ../
-echo "Building Project"
-cmake --build .
-echo "Intalling Project"
-cmake --install .
+
+if [ "$IS_BUILD" -ne 0 ]; then
+    echo "Building Project"
+    cmake --build .
+fi
+
+if [ "$IS_INSTALL" -ne 0 ]; then
+    echo "Intalling Project"
+    cmake --install .
+fi
+
 popd
