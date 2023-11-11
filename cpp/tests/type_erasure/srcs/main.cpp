08346fda4a828d6005f08a82be133f0233761ecd
shayr <shayrashinsky@gmail.com>
2023-11-11 16:08:05 +0200

    tests: add type erasure test

--- cpp/tests/type_erasure/srcs/main.cpp
+++ cpp/tests/type_erasure/srcs/main.cpp
@@ -1,9 +1,9 @@
-#include "../../func_measuring/tester.hpp"
-#include "../../type_erasure/shapes/circle.hpp"
-#include "../../type_erasure/shapes/rect.hpp"
-#include "../../type_erasure/shapes_drawer/shape_drawing.hpp"
-#include "../../type_erasure/unique_ptr_type_erasure/shape_te.hpp"
-#include "../../type_erasure/small_buffer_optimization_type_erasure/shape_te.hpp"
+#include "func_measuring/tester.hpp"
+#include "shapes/circle.hpp"
+#include "shapes/rect.hpp"
+#include "shapes_drawer/shape_drawing.hpp"
+#include "unique_ptr_type_erasure/shape_te.hpp"
+#include "small_buffer_optimization_type_erasure/shape_te.hpp"
 
 #include <vector>
 #include <memory>
@@ -29,11 +29,15 @@ static std::vector<Circle> generatre_circles(uint32_t num_circles) {
     return circles;
 }
 
-static void serialization_test(const std::vector<auto>& serializables, uint32_t num_serializations) {
+
+template<typename T>
+static void serialization_test(const std::vector<T>& serializables, uint32_t num_serializations) {
+    static constexpr auto PRINT_SIZE = 100; // random number
+
     for (uint32_t i = 0; i < num_serializations; i++) {
         for (const auto& serialiable: serializables) {
             std::span<const uint8_t> data = serialiable.serialize();
-            if (data.size() > 100) {
+            if (data.size() > PRINT_SIZE) {
                 printf("gg data.size() > 100\n");
             }
         }
@@ -56,12 +60,12 @@ static void run_shapes_tests(uint32_t num_shapes, uint32_t num_serializations=1)
         const Circle& circle = circles[i/2];
         const Rect& rect = rects[i/2];
 
-        sbo_shapes.push_back(sbo_shape_te::Shape<>(circle, draw_circle));
-        sbo_shapes.push_back(sbo_shape_te::Shape<>(rect, draw_rect));
+        sbo_shapes.emplace_back(circle, draw_circle);
+        sbo_shapes.emplace_back(rect, draw_rect);
 
 
-        up_shapes.push_back(up_shape_te::Shape(circle, draw_circle));
-        up_shapes.push_back(up_shape_te::Shape(rect, draw_rect));
+        up_shapes.emplace_back(circle, draw_circle);
+        up_shapes.emplace_back(rect, draw_rect);
     }
 
     auto serialize_sbo = [&]() {
@@ -77,33 +81,12 @@ static void run_shapes_tests(uint32_t num_shapes, uint32_t num_serializations=1)
 
 }
 
-static void print_going_to(uint32_t allocation_size) {
-    float allocation_size_f = allocation_size;
-    std::string allocation_size_suffix = "Bytes";
-
-    if (allocation_size_f >= 1<<30) {
-        allocation_size_f /= 1<<30;
-        allocation_size_suffix = "GB";
-    }
-
-    if (allocation_size_f >= 1<<20) {
-        allocation_size_f /= 1<<20;
-        allocation_size_suffix = "MB";
-    }
-
-    if (allocation_size_f >= 1<<10) {
-        allocation_size_f /= 1<<10;
-        allocation_size_suffix = "KB";
-    }
-
-    std::string allocation_size_str = _to_string_with_precision(allocation_size_f);
-    printf("\nGoing to test allocation with %s[%s] shapes\n", allocation_size_str.c_str(), allocation_size_suffix.c_str());
-}
-
 int main() {
-    for(uint32_t i = 4; i <= 18; i++) {
+    static constexpr uint8_t TWO_POWER_MIN_ALLOC = 4;
+    static constexpr uint8_t TWO_POWER_MAX_ALLOC = 18;
+    for (uint32_t i = TWO_POWER_MIN_ALLOC; i <= TWO_POWER_MAX_ALLOC; i++) {
         uint32_t allocation_size = 1 << i;
-        print_going_to(allocation_size);
+        print_allocation_size(allocation_size);
         run_shapes_tests(allocation_size, 1);
     }
 }
