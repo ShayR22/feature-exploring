300300378f30f6b64e1b098a4ce3b086cf507402
shayr <shayrashinsky@gmail.com>
2024-06-27 10:01:09 +0300

    cmake_helpers: iwyu: improve log

--- cpp/cmake_helpers/iwyu.cmake
+++ cpp/cmake_helpers/iwyu.cmake
@@ -1,4 +1,4 @@
 if (${USE_IWYU})
-    message(STATUS "setting ${PROJECT_NAME} to use include-what-you-use")
+    message(STATUS "setting ${PROJECT_NAME} to use include-what-you-use IWYU_PATH_AND_OPTIONS=${IWYU_PATH_AND_OPTIONS}")
     set_property(TARGET ${PROJECT_NAME} PROPERTY CXX_INCLUDE_WHAT_YOU_USE ${IWYU_PATH_AND_OPTIONS})
 endif()
